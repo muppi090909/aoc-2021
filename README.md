349c0ebc34b35ff9717bc7a636fc7718def39df8
muppi090909 <whthownothing@gmail.com>
2021-12-03 14:55:11 +0530

    added README

---
+++ README.md
@@ -0,0 +1,2 @@
+**_My programs for advent of code 2021_**
+*what more explanation do you need*
--- day1/main.py
+++ day1/main.py
@@ -1,12 +1,16 @@
 import os
+#init variables
 increased = 0
 pre_i = 0
+#find input file
 file = os.path.split(os.path.realpath(__file__))[0]
 file = os.path.join(file, 'input.txt')
 file = open(file)
+#loop and compare
 for i in file:
     if int(i) > int(pre_i):
         increased += 1
     pre_i = i
+#shut down and show output
 file.close()
 print(f"the depth increased {increased-1} times")
